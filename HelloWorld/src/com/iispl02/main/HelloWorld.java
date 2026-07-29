f0bcbb27820287e75c9148a2651cbb47b8ccb8db
administrator <administrator@iispl>
2026-07-29 11:03:31 +0530

    added one more print statement

--- HelloWorld/src/com/iispl02/main/HelloWorld.java
+++ HelloWorld/src/com/iispl02/main/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Hellooo poojitha");
 		System.out.println("My name is hema");
+		System.out.println("This is my sister name");
 
 	}
 
